b049d5f3ac69f90ef69100cf07b242c64dc7d0e9
viniza <viniza.aquino8108@alumnos.udg.mx>
2023-06-07 15:44:39 -0600

    agregue elif

---
+++ 4-ifelse.py
@@ -0,0 +1,8 @@
+dia = input ("ingresa el dia de la semana")
+dia = "miercoles"
+if dia == "lunes" or dia == "miercoles" or dia == "viernes" :
+    print ("toca curso")
+else :
+    print ("toca descansar")
+print ("adios")
+    
---
+++ 4.1-elif.py
@@ -0,0 +1,9 @@
+dia = input ("ingresa el dia de la semana")
+dia = "viernes"
+if dia == "lunes" or dia == "miercoles" :
+    print ("toca curso")
+elif dia == "viernes" :
+    print ("toca peda")
+else :
+    print ("toca descansar")
+print ("adios")
